dfd082ceab5edeada6807a65c359694a43ae9c79
Mikul958 <michaelpikula958@gmail.com>
2025-10-03 20:42:11 -0400

    Add LineRenderer and render based on input direction

---
+++ Assets/Scripts/BallCollide.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BallCollide : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -5,14 +5,15 @@ public class BallMove : MonoBehaviour
 {
     // Referenced components
     public Rigidbody2D rigidBody;
+    public LineRenderer inputTrailRenderer;
 
     // Constants, set in game engine
-    public float minMouseDiff;      // Minimum distance between center of ball and mouse for a hit to happen.
-    public float maxMouseDiff;      // Maximum distance between center of ball and mouse, further distances will not affect force.
+    public float minMouseDiff;      // Minimum drag distance required for a hit.
+    public float maxMouseDiff;      // Maximum effective drag distance, further drag will not affect force.
 
-    public float minHitSpeed;       // Speed applied to the ball at an input distance of minMouseDiff.
-    public float maxHitSpeed;       // Speed applied to the ball at an input of maxMouseDiff.
-    public float maxSafeThreshold;  // If the ball is moving below this speed, it is considered "safe" and can be hit.
+    public float minHitSpeed;       // Speed applied to the ball at a drag distance of minMouseDiff.
+    public float maxHitSpeed;       // Speed applied to the ball at a drag distance of maxMouseDiff.
+    public float maxSafeSpeed;  // If the ball is moving above this speed, inputs are disabled.
 
     // Instance variables
     private Vector2 respawnPos;
@@ -29,7 +30,7 @@ public class BallMove : MonoBehaviour
     public void handlePlayerInput()
     {
         // Ignore player input if ball is moving too fast
-        if (rigidBody.linearVelocity.magnitude > maxSafeThreshold)
+        if (rigidBody.linearVelocity.magnitude > maxSafeSpeed)
             return;
 
         // Check for user inputs
@@ -49,34 +50,40 @@ public class BallMove : MonoBehaviour
         if (raycastHit.collider != null && raycastHit.collider.gameObject == gameObject)
         {
             isBallClicked = true;
+            inputTrailRenderer.enabled = true;
             Debug.Log("Clicked ball -- Raycast hit at position: " + mousePos);
         }
     }
 
     private void checkForBallRelease()
     {
-        // If not released, update mouse position and 
+        // If not released, update mouse position and input trail
         if (Mouse.current.leftButton.isPressed)
         {
             mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            updateHitIndicator(true);
-
+            updateInputTrail();
             return;
         }
 
         // Else, hit ball and notify level manager
-        updateHitIndicator(false);
+        inputTrailRenderer.enabled = false;
         Vector2 resultVelocity = mapMouseDifferenceToVelocity(mousePos - rigidBody.position);
         if (resultVelocity != Vector2.zero)
         {
             Debug.Log("Added a stroke to counter");  // TODO notify level manager
+            respawnPos = rigidBody.position;
             rigidBody.linearVelocity += resultVelocity;
         }
     }
 
-    private void updateHitIndicator(bool keepActive)
+    private void updateInputTrail()
     {
-        // TODO figure how to draw the line sprite
+        // TODO this is all test code
+        Vector2 mouseDiff = mousePos - rigidBody.position;
+        mouseDiff.Normalize();
+
+        inputTrailRenderer.SetPosition(0, Vector3.zero);
+        inputTrailRenderer.SetPosition(1, new Vector3(mouseDiff.x, mouseDiff.y, 0) * -5);
     }
 
     private Vector2 mapMouseDifferenceToVelocity(Vector2 mouseDiff)
@@ -90,5 +97,15 @@ public class BallMove : MonoBehaviour
 
         Debug.Log("Hit ball at speed: " + resultSpeed);
         return mouseDiff / magnitude * -resultSpeed;  // Normalize mouseDiff vector, then multiply by new speed and invert direction.
+    }
+
+    public void handleOutOfBounds()
+    {
+        // TODO slow down ball + make invisible temporarily, call from BallCollide
+
+        rigidBody.linearVelocity = Vector2.zero;
+        rigidBody.position = respawnPos;
+
+        // TODO respawn timer stuff
     }
 }
