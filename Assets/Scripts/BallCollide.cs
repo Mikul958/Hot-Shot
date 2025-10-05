510031b2b7ada23a114e3d7f021c9f783544184c
Mikul958 <michaelpikula958@gmail.com>
2025-10-05 16:25:28 -0400

    Structure special floor collision checks

--- Assets/Scripts/BallCollide.cs
+++ Assets/Scripts/BallCollide.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class BallCollide : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Referenced components
+    public Transform ballTransform;
+
+    // Constants, set in game engine
+
+    // Instance variables
+    LayerMask floorLayers;
+
     void Start()
     {
-        
+        floorLayers = LayerMask.GetMask("Hole", "Ramp", "Boost", "Terrain");
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        updateTimers();
+        runFloorChecks();
+    }
+
+    private void updateTimers()
     {
         
     }
+    private void runFloorChecks()
+    {
+        Collider2D collider = Physics2D.OverlapPoint(ballTransform.position, floorLayers);
+        if (collider == null)
+            return;
+
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Hole"))
+        {
+            Debug.Log("Level complete!");
+            // TODO notify level manager of completion and do ball velocity stuff
+        }
+        else if (collider.gameObject.layer == LayerMask.NameToLayer("Ramp"))
+        {
+            Debug.Log("Collided with a ramp");
+            // TODO call applyBoost in BallMove based on boost panel direction
+        }
+        else if (collider.gameObject.layer == LayerMask.NameToLayer("Boost"))
+        {
+            Debug.Log("Collided with a boost panel");
+            // TODO call applyBoost in BallMove based on boost panel direction
+        }
+        else if (collider.gameObject.layer == LayerMask.NameToLayer("OutOfBounds"))
+        {
+            Debug.Log("Collided with out of bounds");
+            // TODO call handleOutOfBounds in BallMove
+        }
+        
+        // TODO See if physics material friction can be used for terrain collisions
+    }
+    
+    // Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Ball"), LayerMask.NameToLayer("Walls"), true) -- use this on ramp activation
 }
